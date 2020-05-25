9823498d51768ac34f53163a94c7b96896f06fbf
zouxinjie <zouxinjie126@gmail.com>
2020-05-26 00:22:49 +0800

    初始化阅读器

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span class='hello-cls'>Hello world</span>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang='scss' scoped>
+.hello-cls {
+  color: red
+}
+</style>
